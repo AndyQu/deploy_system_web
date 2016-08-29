cff253842067452e00c052055faac7d3284c4929
quguocheng <quguocheng@meituan.com>
2016-08-29 18:08:44 +0800

    1.加滚动日志 2.加日志级别

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,14 +1,32 @@
 import grails.util.BuildSettings
 import grails.util.Environment
 
+
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} %-5level %logger{0} %class{0} %line - %msg%n"
     }
+	filter(ch.qos.logback.classic.filter.ThresholdFilter) {
+		level = INFO
+	  }
 }
 
-root(INFO, ['STDOUT'])
+appender("ROLLING", ch.qos.logback.core.rolling.RollingFileAppender) {
+	encoder(PatternLayoutEncoder) {
+//	  Pattern = "%d %level %thread %mdc %logger - %m%n"
+		Pattern="%d{HH:mm:ss.SSS} %-5level %logger{0} %class{0} %line - %msg%n"
+	}
+	file="/tmp/docker-deploy/web.log"
+	rollingPolicy(TimeBasedRollingPolicy) {
+	  FileNamePattern = "/tmp/docker-deploy/web-%d{yyyy-MM-dd}.zip"
+	}
+	filter(ch.qos.logback.classic.filter.ThresholdFilter) {
+		level = DEBUG
+	  }
+  }
+
+root(DEBUG, ['STDOUT', 'ROLLING'])
 
 def targetDir = BuildSettings.TARGET_DIR
 if (Environment.isDevelopmentMode() && targetDir) {
